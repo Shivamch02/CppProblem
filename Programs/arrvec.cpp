a379c9e2c9a945f1cdcc4d0939ec0d96a0f79b7d
Shivam Chaurasiya <88436482+Shivamch02@users.noreply.github.com>
2023-08-14 11:38:11 +0530

    Add files via upload

---
+++ Programs/arrvec.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<array>
+#include<vector>
+
+using namespace std;
+int main(){
+
+//Arrays in stl 
+
+    // array<int,4> a = {2,3,4,5};
+    // int size = a.size();
+    // for(int i = 0; i<size; i++){
+    //     cout<<a[i]<<endl;
+    // }
+
+    // cout<<"element of index 2 is :"<<a.at(2)<<endl;
+    // cout<<"empty or not"<<a.empty();
+    // cout<<a.front();
+    // cout<<a.back();
+
+    //Vectors in stl 
+
+    // vector<int> v ;
+    // vector<int> a(5,1);
+    // vector<int> last(a);//copy vector 
+    // for (int i : a){
+    //     cout<<i<<" ";
+    // }
+    // for (int i : last){
+    //     cout<<i<<" ";
+    // }
+
+   
+    // v.push_back(1);
+    // cout<<v.capacity()<<endl;
+    // v.push_back(3);
+    // cout<<v.capacity()<<endl;
+    // v.push_back(5);
+    // cout<<v.capacity()<<endl;
+    // cout<<v.size()<<endl;
+
+    // cout<<v.at(2);
+    // cout<<v.front()<<endl;
+    // cout<<v.back()<<endl;
+    // v.pop_back();
+    // for(int i:v){
+    //     cout<<i<<" ";
+    // }
+}
